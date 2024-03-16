f581d961a7ad495be6ce569db5b999234548c3da
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-03-16 12:00:30 +0100

    Added outputs for fr-01.conllu

--- tests/test.sh
+++ tests/test.sh
@@ -153,6 +153,21 @@ function run_tests {
 				run_test "en-01-10" "CoNLL-U/inputs/en-01.conllu" "CoNLL-U/outputs/en-01-10.hv"	"CoNLL-U" $RFW $RPM
 			fi
 		fi
+
+		if [ "$LANG" == "fr" ]; then
+			if [ "$ID" == "01" ]; then
+				run_test "fr-01-01" "CoNLL-U/inputs/fr-01.conllu" "CoNLL-U/outputs/fr-01-01.hv"	"CoNLL-U" $SMTW
+				run_test "fr-01-02" "CoNLL-U/inputs/fr-01.conllu" "CoNLL-U/outputs/fr-01-02.hv"	"CoNLL-U" $SMTW $RPM
+				run_test "fr-01-03" "CoNLL-U/inputs/fr-01.conllu" "CoNLL-U/outputs/fr-01-03.hv"	"CoNLL-U" $SMTW $RFW
+				run_test "fr-01-04" "CoNLL-U/inputs/fr-01.conllu" "CoNLL-U/outputs/fr-01-04.hv"	"CoNLL-U" $SMTW $RFW $RPM
+				run_test "fr-01-05" "CoNLL-U/inputs/fr-01.conllu" "CoNLL-U/outputs/fr-01-05.hv"	"CoNLL-U" $SMTW $RPM $RFW
+				run_test "fr-01-06" "CoNLL-U/inputs/fr-01.conllu" "CoNLL-U/outputs/fr-01-06.hv"	"CoNLL-U"
+				run_test "fr-01-07" "CoNLL-U/inputs/fr-01.conllu" "CoNLL-U/outputs/fr-01-07.hv"	"CoNLL-U" $RPM
+				run_test "fr-01-08" "CoNLL-U/inputs/fr-01.conllu" "CoNLL-U/outputs/fr-01-08.hv"	"CoNLL-U" $RFW
+				run_test "fr-01-09" "CoNLL-U/inputs/fr-01.conllu" "CoNLL-U/outputs/fr-01-09.hv"	"CoNLL-U" $RPM $RFW
+				run_test "fr-01-10" "CoNLL-U/inputs/fr-01.conllu" "CoNLL-U/outputs/fr-01-10.hv"	"CoNLL-U" $RFW $RPM
+			fi
+		fi
 	fi
 }
 
@@ -214,7 +229,7 @@ if [ $all == 0 ]; then
 else
 	echo "$(date +"%Y/%m/%d.%T") Run all tests" >> $LOG_FILE
 	for f in "CoNLL-U"; do
-		for l in "ca" "es" "en"; do
+		for l in "ca" "es" "en" "fr"; do
 			for i in "01" "02"; do
 				run_tests $f $l $i
 			done
