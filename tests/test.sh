ef1a195f258a73bf4a0129d598844b7408db23dd
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-03-18 15:46:17 +0100

    Sorted alphabetically

--- tests/test.sh
+++ tests/test.sh
@@ -25,8 +25,8 @@ function usage {
 	echo "        are:"
 	echo ""
 	echo "            - ca : Catalan"
-	echo "            - es : Spanish"
 	echo "            - en : English"
+	echo "            - es : Spanish"
 	echo "            - fr : French"
 	echo ""
 	echo "    --id=I : to run the tests that involve input file with ID equal to I."
@@ -129,20 +129,6 @@ function run_tests {
 				run_test "ca-02-10" "CoNLL-U/inputs/ca-02.conllu" "CoNLL-U/outputs/ca-02-10.hv"	"CoNLL-U" $RFW $RPM
 			fi
 		
-		elif [ "$LANG" == "es" ]; then
-			if [ "$ID" == "01" ]; then
-				run_test "es-01-01" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-01.hv"	"CoNLL-U" $SMTW
-				run_test "es-01-02" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-02.hv"	"CoNLL-U" $SMTW $RPM
-				run_test "es-01-03" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-03.hv"	"CoNLL-U" $SMTW $RFW
-				run_test "es-01-04" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-04.hv"	"CoNLL-U" $SMTW $RFW $RPM
-				run_test "es-01-05" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-05.hv"	"CoNLL-U" $SMTW $RPM $RFW
-				run_test "es-01-06" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-06.hv"	"CoNLL-U"
-				run_test "es-01-07" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-07.hv"	"CoNLL-U" $RPM
-				run_test "es-01-08" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-08.hv"	"CoNLL-U" $RFW
-				run_test "es-01-09" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-09.hv"	"CoNLL-U" $RPM $RFW
-				run_test "es-01-10" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-10.hv"	"CoNLL-U" $RFW $RPM
-			fi
-		
 		elif [ "$LANG" == "en" ]; then
 			if [ "$ID" == "01" ]; then
 				run_test "en-01-01" "CoNLL-U/inputs/en-01.conllu" "CoNLL-U/outputs/en-01-01.hv"	"CoNLL-U" $SMTW
@@ -155,6 +141,20 @@ function run_tests {
 				run_test "en-01-08" "CoNLL-U/inputs/en-01.conllu" "CoNLL-U/outputs/en-01-08.hv"	"CoNLL-U" $RFW
 				run_test "en-01-09" "CoNLL-U/inputs/en-01.conllu" "CoNLL-U/outputs/en-01-09.hv"	"CoNLL-U" $RPM $RFW
 				run_test "en-01-10" "CoNLL-U/inputs/en-01.conllu" "CoNLL-U/outputs/en-01-10.hv"	"CoNLL-U" $RFW $RPM
+			fi
+		
+		elif [ "$LANG" == "es" ]; then
+			if [ "$ID" == "01" ]; then
+				run_test "es-01-01" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-01.hv"	"CoNLL-U" $SMTW
+				run_test "es-01-02" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-02.hv"	"CoNLL-U" $SMTW $RPM
+				run_test "es-01-03" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-03.hv"	"CoNLL-U" $SMTW $RFW
+				run_test "es-01-04" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-04.hv"	"CoNLL-U" $SMTW $RFW $RPM
+				run_test "es-01-05" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-05.hv"	"CoNLL-U" $SMTW $RPM $RFW
+				run_test "es-01-06" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-06.hv"	"CoNLL-U"
+				run_test "es-01-07" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-07.hv"	"CoNLL-U" $RPM
+				run_test "es-01-08" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-08.hv"	"CoNLL-U" $RFW
+				run_test "es-01-09" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-09.hv"	"CoNLL-U" $RPM $RFW
+				run_test "es-01-10" "CoNLL-U/inputs/es-01.conllu" "CoNLL-U/outputs/es-01-10.hv"	"CoNLL-U" $RFW $RPM
 			fi
 		
 		elif [ "$LANG" == "fr" ]; then
@@ -233,7 +233,7 @@ if [ $all == 0 ]; then
 else
 	echo "$(date +"%Y/%m/%d.%T") Run all tests" >> $LOG_FILE
 	for f in "CoNLL-U"; do
-		for l in "ca" "es" "en" "fr"; do
+		for l in "ca" "en" "es" "fr"; do
 			for i in "01" "02"; do
 				run_tests $f $l $i
 			done
